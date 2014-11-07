8f2005785b86c2656f2e64550a3f2dfe701c20af
Lutts Cao <lutts.cao@gmail.com>
2014-11-08 00:17:47 +0800

    using google c/c++ style

--- rc/emacs-rc-c-c++.el
+++ rc/emacs-rc-c-c++.el
@@ -6,49 +6,8 @@
 
 ;;; C mode configurations
 
-;; gtags
-;(load (concat my-base-path "rc/emacs-rc-gtags.el"))
-
 ;; xgtags
 (load (concat my-base-path "rc/emacs-rc-xgtags.el"))
-
-(defun my-cc-mode-auto-pair ()
-  (interactive)
-  (make-local-variable 'skeleton-pair-alist)
-  (setq skeleton-pair-alist '(
-                              ;(?` ?` _ "''")
-			      (?` -1 ?{ " " _ " " ?}?, >)
-                              (?\" _ "\"")
-			      (?< _ ">")
-			      (?{ > \n > _ \n ?} >)))
-  (setq skeleton-pair t)
-;(local-set-key (kbd "\"") 'skeleton-pair-insert-maybe)
-  ;(local-set-key (kbd "`") 'skeleton-pair-insert-maybe)
-;(local-set-key (kbd "(") 'skeleton-pair-insert-maybe)
-  (local-set-key (kbd "{") 'skeleton-pair-insert-maybe)
-;(local-set-key (kbd "[") 'skeleton-pair-insert-maybe)
-;(local-set-key (kbd "<") 'skeleton-pair-insert-maybe)
-  )
-
-;(add-hook 'c-mode-common-hook 'my-cc-mode-auto-pair)
-
-;(require 'autopair)
-;(autopair-global-mode)
-
-
-;(defun electric-pair ()
-;      "If at end of line, insert character pair without surrounding spaces.
-;    Otherwise, just insert the typed character."
-;      (interactive)
-;      (if (eolp) (let (parens-require-spaces) (insert-pair)) (self-insert-command 1)))
-;
-;(add-hook 'c-mode-common-hook
-;	  (lambda ()
-;	    (local-set-key (kbd "\"") 'electric-pair)
-;	    (local-set-key (kbd "\'") 'electric-pair)
-;	    (local-set-key (kbd "(") 'electric-pair)
-;	    (local-set-key (kbd "[") 'electric-pair)
-;	    (local-set-key (kbd "{") 'electric-pair)))
 
 ;; automatically delete trailing whitespace when save buffer
 (defun progmodes-hooks ()
@@ -62,7 +21,7 @@
 
 (add-hook 'c-mode-common-hook 'progmodes-hooks)
 
-;; Setting Tab to indent region if anything i selected
+;; Setting Tab to indent region if anything is selected
 (defun tab-indents-region ()
   (local-set-key (kbd "<tab>") 'fledermaus-maybe-tab)
   )
@@ -191,3 +150,7 @@
 (add-hook 'c++-mode-hook 'my-c++-common-hook)
 
 (load (concat my-base-path "rc/emacs-rc-tempo.el"))
+
+;; if using google style, uncomment this line
+(require 'google-c-style)
+(add-hook 'c-mode-common-hook 'google-set-c-style)
